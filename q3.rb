f5464859798f892a9b8fb2ee2a48a0103a3ead63
Aditya <aditya@adityapatawari.com>
2013-07-04 14:03:54 +0530

    fixed the corner cases for roman numerals

--- q3.rb
+++ q3.rb
@@ -8,21 +8,47 @@ while num>0
   if num >= 1000
     roman=roman+'M'
     num=num-1000
+
+  elsif num >= 900
+    roman=roman+'CM'
+    num=num-900
+
   elsif num >= 500
     roman=roman+'D'
     num=num-500
+
   elsif num >= 100
     roman=roman+'C'
     num=num-100
+
+  elsif num >= 90
+    roman=roman+'XC'
+    num=num-90
+
   elsif num >= 50
     roman=roman+'L'
     num=num-50 
+
+  elsif num >= 40
+    roman=roman+'XL'
+    num=num-40
+
   elsif num >= 10
     roman=roman+'X'
     num=num-10
+
+  elsif num >= 9
+    roman=roman+'IX'
+    num=num-9
+
   elsif num >= 5
     roman=roman+'V'
     num=num-5
+
+  elsif num >= 4
+    roman=roman+'IV'
+    num=num-4
+
   else
     roman=roman+'I'
     num=num-1
